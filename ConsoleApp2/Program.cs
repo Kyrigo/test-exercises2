05292f085acc33a09ed899f0f4dfc3a1d46d7a56
Александр Ковалев <a.kovalev@2gis.ru>
2022-03-03 15:45:07 +0300

    добавлена работа с многомерным массивом

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,13 +1,15 @@
-﻿//float.Parse("3.14", NumberStyles.Any, CultureInfo.InvariantCulture, out float result);
-namespace ConsoleApp2;
+﻿namespace ConsoleApp2;
 
-class Program
+internal class Program
 {
     public static void Main(string[] args)
     {
         try
         {
             int selection;
+            Random rnd = new Random();
+            int Min = 0;
+            int Max = 100;
             do
             {
                 Console.WriteLine("Выберите программу\n[1].Вывод массива случайных чисел\n[2]Вывод минимальных значений матрицы\n[3]Калькулятор");
@@ -18,15 +20,12 @@ class Program
             {
                 case 1:
                     int ml;
-                    int Min = 0;
-                    int Max = 100;
 
                     Console.WriteLine("Введите длину массива: ");
                     ml = int.Parse(Console.ReadLine());
 
                     Console.WriteLine("Генерация массива длинной {0}...", ml);
                     int[] numbers;
-                    Random rnd = new Random();
                     numbers = Enumerable.Repeat(0, ml).Select(i => rnd.Next(Min, Max)).ToArray();
                     var stringArray = string.Join(",", numbers);
                     Console.WriteLine("Генерация завершена");
@@ -44,44 +43,73 @@ class Program
                     }
                     break;
                 case 2:
+                    int m, n;
+
+                    Console.WriteLine("Введите длину массива:");
+                    m = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите высоту массива: ");
+                    n = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Генерация многомерного массива [{0}][{1}]...", m, n);
+                    int[,] randomArray = new int[m, n];
+                    for (int i = 0; i < m; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            randomArray[i, j] = rnd.Next(Min, Max);
+                            Console.Write(randomArray[i,j]+"\t");
+                        }
+                        Console.WriteLine("\n");
+                    }
+
+                    Console.WriteLine("Выбираем минимальные значения и сортируем...");
+                    List<int> minArray = new List<int>();
+                    for (int i = 0; i < m; i++)
+                    {
+                        int[] row = Enumerable.Range(0, randomArray.GetLength(1)).Select(x => randomArray[i, x]).ToArray();
+                        minArray.Add(row.Min());
+                    }
+                    
+                    var minArrayString = string.Join(", ", minArray.OrderByDescending(c => c).ToArray());
+
+                    Console.WriteLine("Готовый массив минимальных значений по убыванию: {0}", minArrayString);
                     break;
                 case 3:
-                    int num1; int num2;
+                    float num1; float num2;
                     
-                    Console.WriteLine("Console Calculator in C#\r");
+                    Console.WriteLine("Калькулятор\r");
                     Console.WriteLine("------------------------\n");
                     
-                    Console.WriteLine("Type a number, and then press Enter");
-                    num1 = Convert.ToInt32(Console.ReadLine());
-                    
-                    Console.WriteLine("Type another number, and then press Enter");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-                    
-                    Console.WriteLine("Choose an option from the following list:");
-                    Console.WriteLine("\ta - Add");
-                    Console.WriteLine("\ts - Subtract");
-                    Console.WriteLine("\tm - Multiply");
-                    Console.WriteLine("\td - Divide");
-                    Console.Write("Your option? ");
+                    Console.WriteLine("Введите первое число");
+                    num1 = float.Parse(Console.ReadLine().Replace(".", ","));
+
+                    Console.WriteLine("Введите второе число");
+                    num2 = float.Parse(Console.ReadLine().Replace(".", ","));
+
+                    Console.WriteLine("Выберите операцию:");
+                    Console.WriteLine("\ta - Сложение");
+                    Console.WriteLine("\ts - Вычитание");
+                    Console.WriteLine("\tm - Умножение");
+                    Console.WriteLine("\td - Деление");
                     
                     switch (Console.ReadLine())
                     {
                         case "a":
-                            Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                            Console.WriteLine($"Результат: {num1} + {num2} = " + Math.Round(num1 + num2, 3));
                             break;
                         case "s":
-                            Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                            Console.WriteLine($"Результат: {num1} - {num2} = " + Math.Round(num1 - num2, 3));
                             break;
                         case "m":
-                            Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                            Console.WriteLine($"Результат: {num1} * {num2} = " + Math.Round(num1 * num2, 3));
                             break;
                         case "d":
                             while (num2 == 0)
                             {
-                                Console.WriteLine("Enter a non-zero divisor: ");
+                                Console.WriteLine("Деление на ноль невозможно. Введите другое число: ");
                                 num2 = Convert.ToInt32(Console.ReadLine());
                             }
-                            Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                            Console.WriteLine($"Результат: {num1} / {num2} = " + Math.Round(num1 / num2, 3));
                             break;
                     }
                     break;
@@ -105,6 +133,11 @@ class Program
                     Console.WriteLine("Unknown error - " + e.Message);
                     break;
             }
+        }
+        finally
+        {
+            Console.Write("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
