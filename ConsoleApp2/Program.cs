932ce46060a291e88b8c90293913a1f9d7fae18c
Александр Ковалев <a.kovalev@2gis.ru>
2022-03-16 17:50:53 +0300

    правки

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,118 +9,85 @@ internal class Program
         try
         {
             int selection;
-            Random rnd = new Random();
-            int Min = 0;
-            int Max = 100;
+            var rnd = new Random();
+            const int Min = 0;
+            const int Max = 100;
             do
             {
-                Console.WriteLine("Выберите программу\n[1]Вывод массива случайных чисел\n[2]Вывод минимальных значений матрицы\n[3]Калькулятор");
+                Console.WriteLine("Выберите программу\n[1]Есть ли число в массиве случайных чисел\n[2]Вывод минимальных значений матрицы\n[3]Калькулятор");
                 selection = int.Parse(Console.ReadLine());
-            } while (selection < 0 || selection > 3);
+            } while (selection is < 0 or > 3);
 
             switch (selection)
             {
                 case 1:
-                    int ml;
-
                     Console.WriteLine("Введите длину массива: ");
-                    ml = int.Parse(Console.ReadLine());
+                    var ml = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Генерация массива длинной {0}...", ml);
-                    int[] numbers;
-                    numbers = Enumerable.Repeat(0, ml).Select(i => rnd.Next(Min, Max)).ToArray();
-                    var stringArray = string.Join(",", numbers);
+                    Console.WriteLine($"Генерация массива длинной {ml}...");
+                    var numbers = Enumerable.Repeat(rnd.Next(Min, Max), ml).ToArray();
                     Console.WriteLine("Генерация завершена");
-                    Console.WriteLine("Готовый массив:\n ({0})", stringArray);
+                    Console.WriteLine($"Готовый массив:\n ({string.Join(",", numbers)})");
                     
                     Console.WriteLine("Введите число: ");
-                    int num = int.Parse(Console.ReadLine());
-                    if (numbers.Contains(num))
-                    {
-                        Console.WriteLine("Число {0} есть в массиве", num);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Число {0} отсутствует в массиве", num);
-                    }
+                    var num = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Число {num} {(numbers.Contains(num) ? "есть" : "отсутствует")} в массиве", num);
                     break;
                 case 2:
-                    int m, n;
-
                     Console.WriteLine("Введите длину массива:");
-                    m = int.Parse(Console.ReadLine());
+                    var m = int.Parse(Console.ReadLine());
                     Console.WriteLine("Введите высоту массива: ");
-                    n = int.Parse(Console.ReadLine());
+                    var n = int.Parse(Console.ReadLine());
 
                     Console.WriteLine("Генерация многомерного массива [{0}][{1}]...", m, n);
-                    int[,] randomArray = new int[m, n];
+                    var randomArray = new int[m, n];
                     for (int i = 0; i < m; i++)
                     {
                         for (int j = 0; j < n; j++)
                         {
                             randomArray[i, j] = rnd.Next(Min, Max);
-                            Console.Write(randomArray[i,j]+"\t");
+                            //Console.Write(randomArray[i,j]+"\t");
                         }
-                        Console.WriteLine("\n");
+                        //Console.WriteLine("\n");
                     }
 
                     Console.WriteLine("Выбираем минимальные значения и сортируем...");
-                    List<int> minArray = new List<int>();
-                    for (int i = 0; i < m; i++)
-                    {
-                        int[] row = Enumerable.Range(0, randomArray.GetLength(1)).Select(x => randomArray[i, x]).ToArray();
-                        minArray.Add(row.Min());
-                    }
-                    
-                    var minArrayString = string.Join(", ", minArray.OrderByDescending(c => c).ToArray());
+                    var minArray = Enumerable.Range(0, m)
+                        .Select(i => Enumerable.Range(0, randomArray.GetLength(1)).Select(x => randomArray[i, x]).Min())
+                        .OrderByDescending(c => c).ToArray();
 
-                    Console.WriteLine("Готовый массив минимальных значений по убыванию: {0}", minArrayString);
+                    Console.WriteLine($"Готовый массив минимальных значений по убыванию: {string.Join(", ", minArray)}");
                     break;
                 case 3:
-                    float num1, num2, result;
                     
                     Console.WriteLine("Калькулятор\r");
                     Console.WriteLine("------------------------\n");
                     
                     Console.WriteLine("Введите первое число");
-                    num1 = float.Parse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture);
-
-                    Console.WriteLine("Введите второе число");
-                    num2 = float.Parse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture);
+                    var num1 = float.Parse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture);
 
                     Console.WriteLine("Выберите операцию:");
-                    Console.WriteLine("\ta - Сложение");
-                    Console.WriteLine("\ts - Вычитание");
-                    Console.WriteLine("\tm - Умножение");
-                    Console.WriteLine("\td - Деление");
-                    
-                    switch (Console.ReadLine())
-                    {
-                        case "a":
-                            result = num1 + num2;
-                            Console.WriteLine($"Результат: {result:0.##}");
-                            break;
-                        case "s":
-                            result = num1 - num2;
-                            Console.WriteLine($"Результат: {result:0.##}");
-                            break;
-                        case "m":
-                            result = num1 * num2;
-                            Console.WriteLine($"Результат: {result:0.##}");
-                            break;
-                        case "d":
-                            while (num2 == 0)
-                            {
-                                Console.WriteLine("Деление на ноль невозможно. Введите другое число: ");
-                                num2 = Convert.ToInt32(Console.ReadLine());
-                            }
+                    Console.WriteLine("\t+ - Сложение");
+                    Console.WriteLine("\t- - Вычитание");
+                    Console.WriteLine("\t* - Умножение");
+                    Console.WriteLine("\t/ - Деление");
+                    var operation = Console.ReadLine();
 
-                            result = num1 / num2;
-                            Console.WriteLine($"Результат: {result:0.##}");
-                            break;
-                    }
+                    Console.WriteLine("Введите второе число");
+                    var num2 = float.Parse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture);
+
+                    var result = operation switch
+                    {
+                        "+" => num1 + num2,
+                        "-" => num1 - num2,
+                        "*" => num1 * num2,
+                        "/" when num2 != 0 => num1 / num2,
+                        "/" when num2 == 0 => float.NaN,
+                        _ => throw new Exception("Неизвестная операция"),
+                    };
+                    Console.WriteLine($"Результат: {result:0.##}");
                     break;
-            }
+                    }
         }
         catch (Exception e)
         {
